193e32df12a225379189240674c95307856084b1
Christian Jayson Cantiller <jaysoncan413@Gmail.com>
2025-10-15 16:11:08 +0800

    updated Oauth2ProfileAppApplication.java with startup success message

--- src/main/java/com/cantiller/oauth2_profile_app/Oauth2ProfileAppApplication.java
+++ src/main/java/com/cantiller/oauth2_profile_app/Oauth2ProfileAppApplication.java
@@ -11,7 +11,16 @@ import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 @EnableJpaRepositories(basePackages = "com.cantiller.oauth2_profile_app")
 @ComponentScan(basePackages = "com.cantiller.oauth2_profile_app")
 public class Oauth2ProfileAppApplication {
+
 	public static void main(String[] args) {
 		SpringApplication.run(Oauth2ProfileAppApplication.class, args);
+
+		// Display message after successful startup
+		System.out.println("\n✅ Application started successfully!");
+		System.out.println("----------------------------------------------------");
+		System.out.println("Access the application at: 👉 http://localhost:8080");
+		System.out.println("Access the H2 Console:     👉 http://localhost:8080/h2-console  ");
+		System.out.println("Access the H2 Console:     JDBC URL: jdbc:h2:mem:oauth2db ||  User Name : sa || Password: leave it blank ");
+		System.out.println("----------------------------------------------------\n");
 	}
 }
